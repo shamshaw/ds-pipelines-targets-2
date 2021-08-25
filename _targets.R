62a642580172db972ebb7b919fe2c24765e85da8
Hamshaw <shamshaw@usgs.gov>
2021-08-25 14:36:50 -0400

    Update targets make file to remove configuration parameters temporarily added

--- _targets.R
+++ _targets.R
@@ -5,9 +5,6 @@ source("3_visualize/src/plot_timeseries.R")
 
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse", "dataRetrieval")) # Loading tidyverse because we need dplyr, ggplot2, readr, stringr, and purrr
-p_width <- 12
-p_height <- 7
-p_units <- "in"
 nwis_sites = c("01427207", "01432160", "01435000", "01436690", "01466500")
 
 # Download NWIS data and site info
@@ -57,7 +54,7 @@ p3_targets_list <- list(
   tar_target(
     figure_1_png,
     plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png", site_data_styled,
-                         width = p_width, height = p_height, units = p_units),
+                         ),
     format = "file"
   )
 )
