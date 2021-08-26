2bd0907981a37284cb78a77c425dc9300ac95a45
Hamshaw <shamshaw@usgs.gov>
2021-08-26 13:01:26 -0400

    Revise code to switch some objects between file and object type in makefile

--- _targets.R
+++ _targets.R
@@ -9,46 +9,41 @@ tar_option_set(packages = c("tidyverse", "dataRetrieval")) # Loading tidyverse b
 # Download NWIS data and site info
 p1_targets_list <- list(
   tar_target(
-    site_data_01427207_csv,
-    download_nwis_site_data('1_fetch/out/nwis_data/nwis_data_01427207.csv'),
+    site_data_01427207,
+    download_nwis_site_data(site_num = '01427207')
+  ),
+  tar_target(
+    site_data_01432160,
+    download_nwis_site_data(site_num = '01432160')
+  ),
+  tar_target(
+    site_data_01435000,
+    download_nwis_site_data(site_num = '01435000')
+  ),
+  tar_target(
+    site_data_01436690,
+    download_nwis_site_data(site_num = '01436690')
+  ),
+  tar_target(
+    site_data_01466500,
+    download_nwis_site_data(site_num = '01466500')
+  ),
+  tar_target(
+    combined_nwis_data_csv,
+    combine_data(out_file_path = "2_process/out/combined_nwis_data.csv", list(site_data_01427207, site_data_01432160, site_data_01435000, site_data_01436690, site_data_01466500)),
     format = "file"
   ),
   tar_target(
-    site_data_01432160_csv,
-    download_nwis_site_data('1_fetch/out/nwis_data/nwis_data_01432160.csv'),
-    format = "file"
-  ),
-  tar_target(
-    site_data_01435000_csv,
-    download_nwis_site_data('1_fetch/out/nwis_data/nwis_data_01435000.csv'),
-    format = "file"
-  ),
-  tar_target(
-    site_data_01436690_csv,
-    download_nwis_site_data('1_fetch/out/nwis_data/nwis_data_01436690.csv'),
-    format = "file"
-  ),
-  tar_target(
-    site_data_01466500_csv,
-    download_nwis_site_data('1_fetch/out/nwis_data/nwis_data_01466500.csv'),
-    format = "file"
-  ),
-  tar_target(
-    combined_nwis_data,
-    combine_data(c(site_data_01427207_csv, site_data_01432160_csv, site_data_01435000_csv, site_data_01436690_csv, site_data_01466500_csv)),
-  ),
-  tar_target(
-    site_info_csv,
-    nwis_site_info(fileout = "1_fetch/out/site_info.csv", combined_nwis_data),
-    format = "file"
-  )
+    site_info,
+    nwis_site_info(combined_nwis_data_csv)
+   )
 )
 
 # process and style data to prepare for plotting
 p2_targets_list <- list(
   tar_target(
     site_data_styled, 
-    process_data(combined_nwis_data, site_info_csv)
+    process_data(combined_nwis_data_csv, site_info)
   )
 )
 
@@ -56,8 +51,7 @@ p2_targets_list <- list(
 p3_targets_list <- list(
   tar_target(
     figure_1_png,
-    plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png", site_data_styled,
-                         ),
+    plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png", site_data_styled),
     format = "file"
   )
 )
